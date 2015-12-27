41dbb0cdfb779fe9eca5fbcc3ec833e68bf7d8a4
Matthew McGrath <mjm3853@yahoo.com>
2015-12-27 17:35:00 -0500

    updated from GruntFile.js sample

--- GruntFile.js
+++ GruntFile.js
@@ -1,3 +1,20 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    jshint: {
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      options: {
+        globals: {
+          jQuery: true
+        }
+      }
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint']
+    }
+  });
+
 grunt.registerTask('default', ['qunit_junit', 'qunit']);
 grunt.loadNpmTasks('grunt-contrib-qunit');
    grunt.loadNpmTasks('grunt-qunit-istanbul');
@@ -19,3 +36,6 @@ grunt.loadNpmTasks('grunt-contrib-qunit');
            dest: 'report/'
        }
    };
+
+};
+
